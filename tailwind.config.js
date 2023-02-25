8eb7d501155be16e27343d53adafd592aecd6cf6
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-02-25 13:11:23 -0500

    finished large screen nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*/html'],
+  content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors:{
+        brightBlue: '#233F95',
+        lightGrayishBlue: '#A3B3C9',  
+      },
+    },
   },
   plugins: [],
 }
